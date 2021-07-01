44391baf71bb72450e07d082e0a2b073ab436078
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2021-07-01 13:14:19 +0530

    Final code

---
+++ app.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import numpy as np
+import streamlit as st
+import os
+import matplotlib.pyplot as plt
+
+URI = 'http://127.0.0.1:5000'
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title('Neural Network Visualizer')
+st.sidebar.markdown('# Input Image')
+
+if st.button('Get random predictions'):
+    response = requests.post(URI, data={})
+    response = json.loads(response.text)
+
+    preds = response.get('prediction')
+    image = response.get('image')
+    image = np.reshape(image, (28, 28))
+
+    st.sidebar.image(image, width=150)
+
+    for layer, p in enumerate(preds):
+        numbers = np.squeeze(np.array(p))
+
+        plt.figure(figsize=(32, 4))
+
+        if layer == 4:
+            row = 1
+            col = 10
+        elif layer & 1:
+            row = 1
+            col = 16
+        else:
+            row = 2
+            col = 16
+
+        for i, number in enumerate(numbers):
+            plt.subplot(row, col, i + 1)
+            plt.imshow((number * np.ones((8, 8, 3))
+                        ).astype('float32'))
+            plt.xticks([])
+            plt.yticks([])
+            if layer == 4:
+                plt.xlabel(str(i), fontsize=40)
+        plt.subplots_adjust(wspace=0.05, hspace=0.05)
+        plt.tight_layout()
+
+        st.text('Layer {}'.format(layer + 1), )
+        st.pyplot()
